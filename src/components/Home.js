b3e970815aab00fc69b220ef492e68dd174f965d
陈真 <chazen@qq.com>
2018-03-16 21:36:19 +0800

    [COMMIT] 尝试使用Router

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+Home.propTypes = {
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import dva from 'dva';
-import './index.css';
+/*import dva from 'dva';
+import './index.css';*/
 
 // 1. Initialize
-const app = dva();
+/*const app = dva();*/
 
 // 2. Plugins
 // app.use({});
@@ -11,7 +11,21 @@ const app = dva();
 // app.model(require('./models/example').default);
 
 // 4. Router
-app.router(require('./router').default);
+/*app.router(require('./router').default);*/
 
 // 5. Start
-app.start('#root');
+/*app.start('#root');*/
+// import React from 'react';
+import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Example from "./components/Example";
+import Home from "./components/Home";
+
+ReactDom.render((
+  <BrowserRouter>
+    <div>
+      <Route exact  path='/' component={Home}/>
+      <Route path='/example' component={Example}/>
+    </div>
+  </BrowserRouter>
+  ),document.getElementById("root"));
